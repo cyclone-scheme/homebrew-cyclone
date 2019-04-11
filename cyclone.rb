11506c3101b1b7f64029db58ed2456a68b639de4
Adam Feuer <adam@adamfeuer.com>
2019-04-11 15:12:54 -0700

    working cyclone formula

--- cyclone.rb
+++ cyclone.rb
@@ -1,7 +1,6 @@
 class Cyclone < Formula
   desc ":cyclone: A brand-new compiler that allows practical application development using R7RS Scheme."
   homepage "http://justinethier.github.io/cyclone/"
-  #url "https://github.com/justinethier/cyclone/archive/v0.10.tar.gz"
   url "https://github.com/adamfeuer/cyclone/archive/v0.10.1-test-af1.tar.gz"
   sha256 "8112f249e007bb10475854ac445faa1e41c1dfda7bb0e4c1a70a26c82e7f60fe"
   version "0.10.1-test-af1"
@@ -13,12 +12,23 @@ class Cyclone < Formula
   depends_on :xcode 
 
   def install_cyclone_files
-    bin.install_symlink libexec/"cyclone"
-    bin.install_symlink libexec/"icyc"
-    lib.install_symlink libexec/"libcyclone.a"
+    bin.mkdir
+    include.mkdir
+    lib.mkdir
+    share.mkdir
+    libexec.install %w[scheme srfi include]
+    mkdir libexec/"bin"
+    (libexec/"bin").install "cyclone"
+    (libexec/"bin").install "icyc"
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+    mkdir libexec/"lib"
+    (libexec/"lib").install "libcyclone.a"
+    lib.install_symlink Dir["#{libexec}/lib/*"]
     mkdir include/"cyclone"
     (include/"cyclone").install_symlink Dir["#{libexec}/include/cyclone/*.h"]
-    mkdir_p share/"cyclone/scheme"
+    mkdir share/"cyclone"
+    mkdir share/"cyclone/scheme"
+    mkdir share/"cyclone/scheme/cyclone"
     (share/"cyclone/scheme").install_symlink Dir["#{libexec}/scheme/*.sld"]
     (share/"cyclone/scheme").install_symlink Dir["#{libexec}/scheme/*.o"]
 	(share/"cyclone/scheme").install_symlink Dir["#{libexec}/scheme/*.so"]
@@ -29,14 +39,14 @@ class Cyclone < Formula
     (share/"cyclone/scheme").install_symlink Dir["#{libexec}/scheme/cyclone/array-list.meta"]
     (share/"cyclone/scheme/cyclone").install_symlink Dir["#{libexec}/scheme/cyclone/*.o"]
     (share/"cyclone/scheme/cyclone").install_symlink Dir["#{libexec}/scheme/cyclone/*.so"]
-    mkdir_p share/"cyclone/srfi"
+    mkdir share/"cyclone/srfi"
     (share/"cyclone/srfi").install_symlink Dir["#{libexec}/srfi/*.sld"]
     (share/"cyclone/srfi").install_symlink Dir["#{libexec}/srfi/*.scm"]
-    mkdir_p share/"cyclone/srfi/list-queues"
+    mkdir share/"cyclone/srfi/list-queues"
     (share/"cyclone/srfi/list-queues").install_symlink Dir["#{libexec}/srfi/list-queues/*.scm"]
-    mkdir_p share/"cyclone/srfi/sorting"
+    mkdir share/"cyclone/srfi/sorting"
     (share/"cyclone/srfi/sorting").install_symlink Dir["#{libexec}/srfi/sorting/*.scm"]
-    mkdir_p share/"cyclone/srfi/sets"
+    mkdir share/"cyclone/srfi/sets"
     (share/"cyclone/srfi/sets").install_symlink Dir["#{libexec}/srfi/sets/*.scm"]
     (share/"cyclone/srfi").install_symlink Dir["#{libexec}/srfi/*.meta"]
     (share/"cyclone/srfi").install_symlink Dir["#{libexec}/srfi/*.o"]
@@ -45,6 +55,7 @@ class Cyclone < Formula
 
   def install
     ENV.deparallelize
+    ENV.prepend_path "PATH", "/usr/local/bin"
     system "make"
     install_cyclone_files
   end
